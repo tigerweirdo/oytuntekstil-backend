2ddf232fb3b1d007c77cd91346ab3bd1bab21824
tigerweirdo <temmuzcetiner@gmail.com>
2024-02-13 02:25:47 +0300

    heroku denemesi

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const Borc = require('./models/Borc'); // Borç modelini import et
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // bodyParser.json() yerine express'in kendi metodunu kullan
 
 // MongoDB bağlantısı
-mongoose.connect('mongodb+srv://temmuzcetiner:a94gEjBSfJTaQic5@oytun.vxssfaa.mongodb.net/', {
+mongoose.connect(process.env.MONGODB_URI, { // Çevre değişkeni kullanarak güvenliği artırın
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB bağlantısı başarılı'))
   .catch(err => console.error('MongoDB bağlantı hatası:', err));
+
+// Ana Sayfa için basit bir rota
+app.get('/', (req, res) => {
+    res.send('Hoşgeldiniz! API kullanım bilgileri burada yer alabilir.');
+});
 
 // Borç eklemek için endpoint
 app.post('/api/borclar', async (req, res) => {
@@ -44,16 +46,16 @@ app.get('/api/borclar', async (req, res) => {
 // Bir borcu ödendi olarak işaretleme
 app.patch('/api/borclar/:id', async (req, res) => {
     try {
-        const borc = await Borc.findByIdAndUpdate(req.params.id, { new: true });
+        const borc = await Borc.findByIdAndUpdate(req.params.id, {$set: req.body}, { new: true });
         res.json(borc);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
 });
 
+// Bir borcu güncellemek için endpoint
 app.put('/api/borclar/:id', async (req, res) => {
     try {
-        // Assuming req.body contains the full updated debt object
         const updatedBorc = req.body;
         const borc = await Borc.findByIdAndUpdate(req.params.id, updatedBorc, { new: true });
         if (!borc) {
