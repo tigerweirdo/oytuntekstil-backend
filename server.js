2b437433b1a0dcbf3e0c59d7a69743fe449f5822
tigerweirdo <temmuzcetiner@gmail.com>
2024-02-13 02:37:59 +0300

    heroku deneme.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json()); // bodyParser.json() yerine express'in kendi metodunu kullan
 
 // MongoDB bağlantısı
-mongoose.connect(process.env.MONGODB_URI, { // Çevre değişkeni kullanarak güvenliği artırın
+mongoose.connect('mongodb+srv://temmuzcetiner:a94gEjBSfJTaQic5@oytun.vxssfaa.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB bağlantısı başarılı'))
